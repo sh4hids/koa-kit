ccc02d62db621f360425c8bf7238308e61fd210a
sh4hids <shahid.bracu@gmail.com>
2018-11-07 16:01:53 +0600

    Changed router file

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,5 @@ const CONFIG = require('../config');
 module.exports = router => {
   router.prefix(`/${CONFIG.apiVersion}`);
   router.use('/', require('../components/api/v1'));
-  router.use('/users', require('../components/users'));
+  router.use('/users', require('../components/users/users.route'));
 };
