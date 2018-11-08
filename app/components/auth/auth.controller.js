0ce3a060a47d30b05437a2aec6aaa47f761e0503
sh4hids <shahid.bracu@gmail.com>
2018-11-08 10:06:28 +0600

    Added log in and logout

--- app/components/auth/auth.controller.js
+++ app/components/auth/auth.controller.js
@@ -1,3 +1,33 @@
 const passport = require('koa-passport');
 
-module.exports = {};
+async function logIn(ctx, next) {
+  return passport.authenticate('local', async (err, user, info) => {
+    if (user === false) {
+      ctx.status = 401;
+      ctx.body = info.message;
+    } else {
+      try {
+        // const { accessToken, refreshToken } = await generateTokens({user}, "secret");
+        ctx.login(user);
+        ctx.body = {
+          success: true,
+          message: "You've successfully logged in.",
+        };
+      } catch (e) {
+        ctx.throw(500, e);
+      }
+    }
+  })(ctx, next);
+}
+
+async function logOut(ctx) {
+  if (ctx.isAuthenticated()) {
+    ctx.logout();
+    ctx.redirect('/api/v1/');
+  } else {
+    ctx.body = { success: false };
+    ctx.throw(401);
+  }
+}
+
+module.exports = { logIn, logOut };
--- app/components/auth/auth.route.js
+++ app/components/auth/auth.route.js
@@ -3,22 +3,8 @@ const passport = require('koa-passport');
 const router = new Router();
 const controller = require('./auth.controller');
 
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/api/v1/users',
-    failureRedirect: '/',
-  })
-);
+router.post('/login', controller.logIn);
 
-router.get('/logout', async ctx => {
-  if (ctx.isAuthenticated()) {
-    ctx.logout();
-    ctx.redirect('/api/v1/');
-  } else {
-    ctx.body = { success: false };
-    ctx.throw(401);
-  }
-});
+router.get('/logout', controller.logOut);
 
 module.exports = router.routes();
